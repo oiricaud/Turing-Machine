9b85ce9e6a99c32029f291facd45ade4b8c247e8
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-21 12:23:39 -0700

    Define the methods that are necessary for the user to fill for the Turing machine.

--- src/TuringMachine.java
+++ src/TuringMachine.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  Homework: I lost count.
  @author : Oscar Ivan Ricaud.
@@ -39,17 +41,41 @@ public class TuringMachine {
 
     public static void main(String[] args) {
         // Initialize turing machine.
-        intializeSettings(3);
+        String input = "1010";
+        intialize_Settings(input.length());
     }
 
-    public static void intializeSettings(int i) {
-        int[] num_of_states = new int[i];
-        int[] num_of_m_symbols = new int[i];
+    public static void intialize_Settings(int i) {
+        // Get the name of the states from the user
+        int[] num_of_states = new int[7];
+        fill_up_da_states(num_of_states);
+
+        // Get the name of the symbols from the user
+        int[] num_of_m_symbols = new int[3];
+        fill_up_da_symbols(num_of_m_symbols);
+
         int[][] what_to_do = new int[num_of_states.length][num_of_m_symbols.length];
         int[][] what_shall_we_write = new int[num_of_states.length][num_of_m_symbols.length];
         int[][] where_to_move = new int[num_of_states.length][num_of_m_symbols.length];
         int[] original_contents_of_tape = new int [100];
         int reader = 0;
+    }
 
+    /**
+     * @param num_of_m_symbols is the number of M symbols.
+     *  In this method the user will fill up the name of the symbols.
+     */
+    private static void fill_up_da_symbols(int[] num_of_m_symbols) {
+    }
+
+    /**
+     * @param num_of_states is the number of states the state machine has
+     *  In this method the user will fill up the name of the states.
+     */
+    private static void fill_up_da_states(int[] num_of_states) {
+        Scanner input = new Scanner(System.in);
+        for(int i = 0 ; i < num_of_states.length; i++){
+            System.out.println("for q" + i + ":" + input.next());
+        }
     }
 }
